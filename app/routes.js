d553e06cafd69fa59c9c8a12a422638004760747
Jas Johal <JSJohal@users.noreply.github.com>
2020-06-25 16:15:33 +0100

    added v5

--- app/routes.js
+++ app/routes.js
@@ -7,5 +7,6 @@ const router = express.Router()
   require('./routes/2-0/v2.js')(router);
   require('./routes/3-0/v3.js')(router);
   require('./routes/4-0/v4.js')(router);
+  require('./routes/5-0/v5.js')(router);
 
 module.exports = router
